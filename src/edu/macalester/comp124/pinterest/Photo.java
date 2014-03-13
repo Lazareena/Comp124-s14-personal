0350100cb0574ee3edc8c36bb51eba4331cb637f
Reena <lthaveet@macalester.edu>
2014-03-13 10:59:23 -0500

    HW3

--- src/edu/macalester/comp124/pinterest/Photo.java
+++ src/edu/macalester/comp124/pinterest/Photo.java
@@ -4,6 +4,9 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 import javafx.scene.paint.Color;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 /**
  * Created by Reena on 3/8/14.
  */
@@ -40,7 +43,35 @@ public class Photo extends GraphicsProgram {
         Circles h = new Circles(3,3);
         add(h, (getWidth()*3.1)/4 , (getHeight()*7)/8);
 
+        addMouseListeners();
     }
+
+    public  void mouseClicked(MouseEvent e) {
+        if(e.getX() % 2 == 0 && e.getY() % 2 == 0) {
+        Circles add2Circle = new Circles(3,3);
+        add(add2Circle, e.getX(), e.getY());
+        } else { Circles addCircle = new Circles(5,5);
+        add(addCircle, e.getX(), e.getY());
+    }
+    }
+
+                 GObject obj;
+                 double lastX, lastY;
+                 @Override
+                 public void mousePressed(MouseEvent e) {
+                     lastX =e.getX();
+                     lastY =e.getY();
+                     obj = getElementAt(lastX, lastY);
+                 }
+
+                 @Override
+                 public void mouseDragged(MouseEvent e) {
+                     if ( obj != null) {
+                         obj.move(e.getX() - lastX, e.getY()- lastY);
+                         lastX = e.getX();
+                         lastY = e.getY();
+                     }
+                 }
 
 }
 
